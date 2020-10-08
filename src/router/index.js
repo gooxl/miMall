600f4b75e24ecc9abd020e77550bf3b6d1bdf7c6
guxl <nicgu@qq.com>
2020-10-08 09:57:33 +0800

    创建项目

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Index from '../views/index.vue'
+import Product from '../views/product.vue'
+import Detail from '../views/detail.vue'
+import Cart from '../views/cart.vue'
+import Order from '../views/order.vue'
+import OrderConfirm from '../views/orderConfirm.vue'
+import OrderList from '../views/orderList.vue'
+import OrderPay from '../views/orderPay.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/',name: 'Home',component: Home,redirect:'/index',
+    children:[
+      {path:'/index',name:'index',component:Index},
+      {path:'/product/:id',name:'product',component:Product},
+      {path:'/detail/:id',name:'detail',component:Detail},
+    ]
+  },
+  {path:'/order',name:'order',component:Order,
+    children:[
+      {path:'/list',name:'order-list',component:OrderList},
+      {path:'/confirm',name:'order-confirm',component:OrderConfirm},
+      {path:'/pay',name:'order-pay',component:OrderPay},
+    ]
+  },
+  {path:'/cart',name:'cart',component:Cart},
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    home
+    <nav-header></nav-header>
+    <router-view/>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import navHeader from '@/components/NavHeader'
+import navFooter from '@/components/NavFooter'
+export default {
+  name: 'Home',
+  components: {
+    navHeader,
+    navFooter
+  }
+}
+</script>
